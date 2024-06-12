e8012aa167b1bbc29a659bade5f42f987a326233
Blobby-py <thomas3sep@hotmail.com>
2024-06-12 13:07:24 +0200

    api.php changed

--- DayPrepDB/routes/api.php
+++ DayPrepDB/routes/api.php
@@ -20,8 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('/tasks/store', [TaskController::class, 'store']);
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
-// Route::get('/auth/me', [AuthController::class, 'me']);
-// Route::middleware('auth:sanctum')->get('/auth/me', [AuthController::class, 'me']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
